031a716944f00f5e7471d02e619042f7c7fac9ec
brtheo <brossiertheo@live.fr>
2022-06-12 13:15:37 +0200

    shade generator

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,8 @@ interface IData {
 type DataValue = String | Number | Boolean | BigInteger | any
 
 type InputType = 'text' | 'checkbox' | 'number'
+
+interface RegExpMatchArraySafe extends Array<string|number> {
+  index?: number;
+  input?: string;
+}
